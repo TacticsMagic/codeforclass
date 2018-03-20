96bf3687eb2ab3aa24879f4b3b205801dae69ae9
Ebenezer Ramos <eramos.sh@mysummitps.org>
2018-03-20 16:08:51 +0000

    first commit

---
+++ int3checkpoint/info.js
@@ -0,0 +1,11 @@
+var P5 = document.getElementById("P5");
+var JoJo = document.getElementById("JoJo");
+var display = document.getElementById("display");
+
+P5.addEventListener("click",function(){
+    display.innerHTML = "Persona 5 is a game where you play as a "
+});
+
+JoJo.addEventListener("click",function(){
+    display.innerHTML = ""
+});
